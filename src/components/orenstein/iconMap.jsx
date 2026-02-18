577ce0b6af64f4ec0c980473754551b223175706
claudio orenstein <clauorenstein@gmail.com>
2026-02-18 23:58:54 +0000

    File changes

--- src/components/orenstein/iconMap.jsx
+++ src/components/orenstein/iconMap.jsx
@@ -1,9 +1,27 @@
+
 import React from 'react';
-import { Briefcase, Layers, Box, LayoutGrid } from 'lucide-react';
+import { 
+  Briefcase, 
+  Layers, 
+  Box, 
+  LayoutGrid, 
+  Package, 
+  Boxes, 
+  FolderKanban, 
+  Server, 
+  Database, 
+  Globe 
+} from 'lucide-react';
 
 export const ICON_MAP = {
   Briefcase: <Briefcase className="w-5 h-5" />,
   Layers: <Layers className="w-5 h-5" />,
   Box: <Box className="w-5 h-5" />,
-  LayoutGrid: <LayoutGrid className="w-5 h-5" />
+  LayoutGrid: <LayoutGrid className="w-5 h-5" />,
+  Package: <Package className="w-5 h-5" />,
+  Boxes: <Boxes className="w-5 h-5" />,
+  FolderKanban: <FolderKanban className="w-5 h-5" />,
+  Server: <Server className="w-5 h-5" />,
+  Database: <Database className="w-5 h-5" />,
+  Globe: <Globe className="w-5 h-5" />
 };
